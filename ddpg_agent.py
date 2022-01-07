a777b9afbf7c288ce9a9eca4cd3fd2e0c75c6e61
Victor Nascimento <victormatheusnasc@gmail.com>
2022-01-07 02:14:51 -0300

    Fix the seed in the training

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -159,6 +159,7 @@ class OUNoise:
         self.theta = theta
         self.sigma = sigma
         self.seed = random.seed(seed)
+        self.seed2 = np.random.seed(seed)
         self.reset()
 
     def reset(self):
